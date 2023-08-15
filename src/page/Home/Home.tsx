693e904161ae2c1474bf92d5b422cccaa1d7e777
Maxence Maze au <maxencemazeau@Maxences-MacBook-Air.local>
2023-08-15 15:22:12 +0200

    Features + footer + hero + navbar + pricing

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -1,10 +1,21 @@
 import React from "react"
+import NavBar from "./NavBar"
+import Hero from "./Hero"
+import Pricing from "./Pricing"
+import Footer from "./Footer"
+import Features from "./Features"
+import VisualFeature from "./VisualFeature"
 
 export default function Home (){
 
     return(
         <>
-            <h1>Welcome to the Home Page</h1>
+            <NavBar />
+            <Hero />
+            <VisualFeature />
+            <Features />
+            <Pricing />
+            <Footer />
         </>
     )
 }   
