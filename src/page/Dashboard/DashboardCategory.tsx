d07b8c327facd67a6b286900d2b87c5a3430515c
Maxence Maze au <maxencemazeau@Maxences-Air.hitronhub.home>
2023-08-25 16:02:58 -0400

    test

--- src/page/Dashboard/DashboardCategory.tsx
+++ src/page/Dashboard/DashboardCategory.tsx
@@ -1,63 +1,56 @@
-import React from "react"
+import React from "react";
 import CategoryCard from "../../components/CategoryCards"
 
-interface integrations{
-    title : string,
+interface integrations {
+    title: string,
     percent: number,
     budget: number,
-    spend : number
+    spend: number
 }
 
-const IntegrationProps : integrations[] = [
+const IntegrationProps: integrations[] = [
     {
         title: "Groceries",
         percent: 86,
         budget: 400,
         spend: 300
-
-    }, {
+    },
+    {
         title: "House",
         percent: 86,
         budget: 400,
         spend: 300
-
-    }, {
+    },
+    {
         title: "Game",
         percent: 86,
         budget: 1000,
         spend: 2000
-
     },
     {
         title: "Figma",
         percent: 86,
         budget: 10,
         spend: 5
-
     },
 ]
 
-const DashboardCategory: React.FC= () =>{
-
-return(
-    <>
-    <section className="py-14">
-                <div className="w-full mx-auto px-4 md:px-8">
-                <ul className="grid gap-8 sm:grid-cols-2 lg:grid-cols-4">    
-                {
-                    IntegrationProps.map((item, idx: number) => (
-                        <CategoryCard
-                            key={idx}
-                            category={{title : item.title, percent: item.percent, budget: item.budget, spend: item.spend}}
-                        />
-                    ))
-                }
-                </ul>
-            </div>
-           </section> 
-
-    </>
-)
+const DashboardCategory: React.FC = () => {
+    return (
+        <section className="py-14 flex w-full justify-center">
+            {
+                IntegrationProps.map((item, idx: number) => (
+                    <div className="w-1/4 mr-8">
+                    <CategoryCard
+                        key={idx}
+                        category={{ title: item.title, percent: item.percent, budget: item.budget, spend: item.spend }}
+                    />
+                    </div>
+                ))
+                
             }
+        </section>
+    )
+}
 
 export default DashboardCategory;
--- src/page/Dashboard/DashboardHome.tsx
+++ src/page/Dashboard/DashboardHome.tsx
@@ -1,27 +1,35 @@
 import React, { useState } from "react";
 import DashboardNavBar from "./DashboardNavBar";
 import DashboardCategory from './DashboardCategory';
+import DashboardChart from './DashboardChart';
 
-export default function DashboardHome(){
+export default function DashboardHome() {
 
     const [headerHeight, setHeaderHeight] = useState("h-16");
 
-  const handleHeaderHeight = (isExpanded: any) => {
-    if (isExpanded) {
-      setHeaderHeight("h-64");
-    } else {
-      setHeaderHeight("h-16");
-    }
-  };
+    const handleHeaderHeight = (isExpanded: any) => {
+        if (isExpanded) {
+            setHeaderHeight("h-64");
+        } else {
+            setHeaderHeight("h-16");
+        }
+    };
 
-    return(
-        <div className="flex flex-col lg:flex-row">
-        <div className={`rounded-lg bg-gray-100 ${headerHeight} lg:fixed`}>
-            <DashboardNavBar  handleHeight={handleHeaderHeight} />
+    return (
+      <>  
+      <div className={`rounded-lg bg-gray-100 ${headerHeight} lg:fixed`}>
+                <DashboardNavBar handleHeight={handleHeaderHeight} />
+            </div>
+        <div className="grid grid-cols-5 gap-4">
+            <div className="col-start-2 col-end-5 gap-4">
+                <div className="grow">
+                    <DashboardCategory />
+                </div>
+                <div className="grow">
+                    <DashboardChart />
+                </div>
+            </div>
         </div>
-        <div className="flex flex-col col-2 grow lg:ml-40">
-        <DashboardCategory />
-        </div>
-        </div>
-    )
+      </>
+    );
 }
